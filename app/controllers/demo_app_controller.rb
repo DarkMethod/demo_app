9027ea0c78e1504dc54fc64bba85f47eae7d414d
DarkMethod <mrmarkam@gmail.com>
2014-09-29 03:26:07 +0530

    Added 3 static pages

---
+++ app/controllers/demo_app_controller.rb
@@ -0,0 +1,10 @@
+class DemoAppController < ApplicationController
+  def signup
+  end
+
+  def reg_users
+  end
+
+  def user_info
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'demo_app/signup'
+
+  get 'demo_app/reg_users'
+
+  get 'demo_app/user_info'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
